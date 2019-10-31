8b54c365ac61a61adc4fa55c5436d5cb8b77b7d8
mwzaman <mozaman.knit@gmail.com>
2019-10-31 14:49:28 +0600

    Add edit, show, delete and index pates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -13,8 +23,26 @@ class ArticlesController < ApplicationController
       render 'new'
   end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated!'
+      redirect_to article_path(@article)
+    else
+    render "edit"
+  end
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+   @article = Article.find(params[:id])
+   @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
   end
 
   private
